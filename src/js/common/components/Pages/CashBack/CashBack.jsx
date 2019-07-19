0c982019277393944a4439dc5d2e6fa47853e6c9
bogdan tarasenko <bogdanfrontend@gmail.com>
2019-07-19 18:18:57 +0300

    feat: cashback page

--- src/js/common/components/Pages/CashBack/CashBack.jsx
+++ src/js/common/components/Pages/CashBack/CashBack.jsx
@@ -1,18 +1,14 @@
 import React, { Component } from 'react'
-// import { Player, BigPlayButton } from 'video-react';
 import 'video-react/dist/video-react.css';
 import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
-// import CurrencyProtectInfo from './CurrencyProtectInfoComponent/CurrencyProtectInfo'
-// import HowItWorks from './HowItWorksComponent/HowItWorks'
-// import ItsSimple from './ItsSimpleComponent/ItsSimple'
+import Button from '@material-ui/core/Button'
 import Slider from '../../SliderComponent/Slider'
 import Footer from '../../Footer/Footer'
-// import Testimonial from './TestimonialComponent/Testimonial'
-import mobile from '../../../../../assets/images/icon-mobile-white.svg'
-import email from '../../../../../assets/images/icon-email.svg'
-
 import styles from './CashBack.css'
+import amazon from '../../../../../assets/images/amazon.png';
+import netflix from '../../../../../assets/images/netflix.png';
+import tmobile from '../../../../../assets/images/tmobile.png';
 
 class Partners extends Component {
   render() {
@@ -24,42 +20,68 @@ class Partners extends Component {
             <Grid container>
               <Grid item xs={12} sm={12}>
                 <div className={styles.titleBlock}>
-                  <p className={styles.title}>Our Partners</p>
-                  <p className={styles.subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam blandit nec magna vel pretium. Curabitur euismod, metus id aliquam tincidunt, nisl ipsum tincidunt massa, sed scelerisque est risus quis neque.</p>
+                  <p className={styles.title}>Campus Cashback</p>
+                  <p className={styles.subtitle}>Experience the best rewards on your campus and across the country</p>
+                </div>
+              </Grid>
+            </Grid>
+            <Grid container>
+              <Grid item xs={12} sm={4}>
+                <div className={styles.rewardWrapper}>
+                    <div className={styles.rewardImage}>
+                        <img src={amazon} />
+                    </div>
+                    <div className={styles.rewardText}>
+                        <p>1 month of Netflix</p>
+                        <p className={styles.subtitleRed}>Free*</p>
+                    </div>
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <div className={styles.rewardWrapper}>
+                    <div className={styles.rewardImage}>
+                        <img src={netflix} />
+                    </div>
+                    <div className={styles.rewardText}>
+                        <p>1 month of Netflix</p>
+                        <p className={styles.subtitleRed}>Free*</p>
+                    </div>
+                </div>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <div className={styles.rewardWrapper}>
+                    <div className={styles.rewardImage}>
+                        <img src={tmobile} />
+                    </div>
+                    <div className={styles.rewardText}>
+                        <p>
+                            <span className={styles.redText}>Discounts</span> on Cell Bill
+                        </p>
+                    </div>
+                </div>
+              </Grid>
+            </Grid>
+            <div className={styles.spacing} />
+            <Grid container>
+              <Grid item xs={12} sm={12}>
+                <p className={styles.thumbnail}>Enter your college email to see all the exclusive benefits you can get on campus with North Loop.</p>
+                <div className={styles.formWrapper}>
+                    <form action="" className={styles.form}>
+                        <input type="text" placeholder="Enter your email" className={styles.formInput} />
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            className={styles.button}
+                        >
+                            Sign Up
+                        </Button>
+                    </form>
                 </div>
               </Grid>
             </Grid>
           </Container>
         </div>
         <div id="2">
-          <Container className={styles.main}>
-            <Grid container>
-              <Grid item xs={12} sm={6}>
-                <div className={styles.titleBlock}>
-                  <p className={styles.subtitleRed}>Interested in becoming a partner?</p>
-                  <p className={styles.title}>Contact Us</p>
-                </div>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <div className={styles.contactWrapper}>
-                    <Container className={styles.footerSection}>
-                        <div className={styles.innerContainer}>
-                            <img src={mobile} alt="icon message" />
-                            <p className={styles.text}>+1(831)740-8126</p>
-                        </div>
-                    </Container>
-                    <Container className={styles.message}>
-                        <div className={styles.innerContainer}>
-                            <img src={email} alt="icon message" />
-                            <p className={styles.text}>help@nolobank.com</p>
-                        </div>
-                    </Container>
-                </div>
-              </Grid>
-            </Grid>
-          </Container>
-        </div>
-        <div id="4">
           <Footer />
         </div>
       </Slider>
